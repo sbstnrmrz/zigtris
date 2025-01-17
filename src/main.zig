bd499018997af2b05ffd9fa0519e95bdf30640bc
sbstnrmrz <ramicastasebas@gmail.com>
2025-01-17 17:53:11 -0400

    added basic 2d batch rendering?

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,14 @@
-//------------------------------------------------------------------------------
-//  quad.zig
-//
-//  Simple 2D rendering with vertex- and index-buffer.
-//------------------------------------------------------------------------------
 const std = @import("std");
 const sokol = @import("sokol");
 const slog = sokol.log;
 const sg = sokol.gfx;
 const sapp = sokol.app;
 const sglue = sokol.glue;
+const stime = sokol.time;
 const m = @import("math.zig");
 const shader_quad = @import("shaders/quad.glsl.zig");
 const Vec4 = m.Vec4;
+const ArrayList = std.ArrayList;
 
 const print = std.debug.print;
 
@@ -24,12 +21,37 @@ const state = struct {
     var pip: sg.Pipeline = .{};
     var pass_action: sg.PassAction = .{};
     const vs_params: shader_quad.VsParams = .{ .p = ortho_proj_mat };
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var quad_vertices = ArrayList(f32).init(allocator);
+    var quad_indices = ArrayList(u16).init(allocator);
+    var quad_count: u32 = 0;
 };
 
+fn RGBToFloat(c: u8) f32 {
+    return @as(f32, @floatFromInt(c)) / 255.0;
+}
+
+fn randColor() ColorRGB {
+    var prng = std.Random.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        std.posix.getrandom(std.mem.asBytes(&seed)) catch unreachable;
+        break :blk seed;
+    });
+    const rand = prng.random();
+
+    var res = ColorRGB{};
+    res.r = rand.intRangeAtMost(u8, 0, 255);
+    res.g = rand.intRangeAtMost(u8, 0, 255);
+    res.b = rand.intRangeAtMost(u8, 0, 255);
+
+    return res;
+}
+
 const ColorRGB = struct {
-    r: u8,
-    g: u8,
-    b: u8,
+    r: u8 = 0,
+    g: u8 = 0,
+    b: u8 = 0,
 
     fn redToFloat(self: *ColorRGB) f32 {
         return @as(f32, @floatFromInt(self.r)) / 255.0;
@@ -42,33 +64,49 @@ const ColorRGB = struct {
     }
 };
 
-fn mat4ToArr(mat: m.Mat4) [16]f32 {
-    var res: [16]f32 = undefined;
+const Quad = struct {
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
+};
 
-    var i: usize = 0;
-    for (mat.m) |row| {
-        for (row) |col| {
-            res[i] = col;
-            i += 1;
-        }
+fn render_quad(quad: Quad, color: ColorRGB) void {
+    const vertices = [_]f32{
+        quad.x,          quad.y,          0.0, RGBToFloat(color.r), RGBToFloat(color.g), RGBToFloat(color.b), 1.0,
+        quad.x + quad.w, quad.y,          0.0, RGBToFloat(color.r), RGBToFloat(color.g), RGBToFloat(color.b), 1.0,
+        quad.x,          quad.y + quad.h, 0.0, RGBToFloat(color.r), RGBToFloat(color.g), RGBToFloat(color.b), 1.0,
+        quad.x + quad.h, quad.y + quad.h, 0.0, RGBToFloat(color.r), RGBToFloat(color.g), RGBToFloat(color.b), 1.0,
+    };
+
+    for (vertices) |v| {
+        state.quad_vertices.append(v) catch unreachable;
     }
 
-    return res;
+    state.quad_indices.append((@as(u16, @intCast(state.quad_count)) * 4) + 0) catch unreachable;
+    state.quad_indices.append((@as(u16, @intCast(state.quad_count)) * 4) + 1) catch unreachable;
+    state.quad_indices.append((@as(u16, @intCast(state.quad_count)) * 4) + 2) catch unreachable;
+    state.quad_indices.append((@as(u16, @intCast(state.quad_count)) * 4) + 2) catch unreachable;
+    state.quad_indices.append((@as(u16, @intCast(state.quad_count)) * 4) + 1) catch unreachable;
+    state.quad_indices.append((@as(u16, @intCast(state.quad_count)) * 4) + 3) catch unreachable;
+
+    state.quad_count += 1;
 }
 
 export fn init() void {
+    stime.setup();
     sg.setup(.{
         .environment = sglue.environment(),
         .logger = .{ .func = slog.func },
     });
 
     // a vertex buffer
-    state.bind.vertex_buffers[0] = sg.makeBuffer(.{ .usage = .DYNAMIC, .size = 1000, .type = .VERTEXBUFFER });
+    state.bind.vertex_buffers[0] = sg.makeBuffer(.{ .size = 500000, .usage = .DYNAMIC, .type = .VERTEXBUFFER });
 
-    // an index buffer
     state.bind.index_buffer = sg.makeBuffer(.{
+        .size = 8192,
         .type = .INDEXBUFFER,
-        .data = sg.asRange(&[_]u16{ 0, 1, 2, 2, 1, 3 }),
+        .usage = .DYNAMIC,
     });
 
     // a shader and pipeline state object
@@ -83,7 +121,6 @@ export fn init() void {
         .index_type = .UINT16,
     });
 
-    // clear to black
     state.pass_action.colors[0] = .{
         .load_action = .CLEAR,
         .clear_value = .{ .r = 0, .g = 0, .b = 0, .a = 1 },
@@ -91,26 +128,35 @@ export fn init() void {
 }
 
 export fn frame() void {
+    if (state.quad_vertices.items.len > 0) {
+        state.quad_vertices.clearRetainingCapacity();
+        state.quad_indices.clearRetainingCapacity();
+        state.quad_count = 0;
+        state.bind.vertex_buffer_offsets[0] = 0;
+        state.bind.index_buffer_offset = 0;
+    }
+
     sg.beginPass(.{ .action = state.pass_action, .swapchain = sglue.swapchain() });
 
-    const width = 32.0;
-    const height = 32.0;
-    const rect_x = 100.0;
-    const rect_y = 100.0;
-    var color = ColorRGB{ .r = 255, .g = 0, .b = 0 };
+    for (0..25) |i| {
+        for (0..25) |j| {
+            const _i = @as(f32, @floatFromInt(i));
+            const _j = @as(f32, @floatFromInt(j));
+            render_quad(.{ .x = 32 * _j, .y = 32 * _i, .w = 32, .h = 32 }, randColor());
+        }
+    }
 
-    sg.updateBuffer(state.bind.vertex_buffers[0], sg.asRange(&[_]f32{
-        rect_x,         rect_y,          0.0, color.redToFloat(), color.greenToFloat(), color.blueToFloat(), 1.0,
-        rect_x + width, rect_y,          0.0, color.redToFloat(), color.greenToFloat(), color.blueToFloat(), 1.0,
-        rect_x,         rect_y + height, 0.0, color.redToFloat(), color.greenToFloat(), color.blueToFloat(), 1.0,
-        rect_x + width, rect_y + height, 0.0, color.redToFloat(), color.greenToFloat(), color.blueToFloat(), 1.0,
-    }));
+    if (state.quad_vertices.items.len > 0) {
+        sg.updateBuffer(state.bind.vertex_buffers[0], sg.asRange(state.quad_vertices.items));
+        sg.updateBuffer(state.bind.index_buffer, sg.asRange(state.quad_indices.items));
+    }
+
+    print("quads: {d}\nvertices: {d}\nindices: {d}\n\n", .{ state.quad_count, state.quad_vertices.items.len, state.quad_indices.items.len });
 
     sg.applyPipeline(state.pip);
     sg.applyBindings(state.bind);
     sg.applyUniforms(shader_quad.UB_vs_params, sg.asRange(&state.vs_params));
-
-    sg.draw(0, 6, 1);
+    sg.draw(0, state.quad_count * 6, 1);
     sg.endPass();
     sg.commit();
 }
@@ -130,37 +176,18 @@ export fn input(e: ?*const sapp.Event) void {
 
 export fn cleanup() void {
     sg.shutdown();
+    state.quad_vertices.deinit();
+    state.quad_indices.deinit();
+    _ = state.gpa.deinit();
 }
 
-fn render_quad(x: f32, y: f32, width: f32, height: f32, color: ColorRGB) void {
-    const quad = [_]f32{
-        x,         y,          0.0, color.redToFloat(), color.greenToFloat(), color.blueToFloat(), 1.0,
-        x + width, y,          0.0, color.redToFloat(), color.greenToFloat(), color.blueToFloat(), 1.0,
-        x,         y + height, 0.0, color.redToFloat(), color.greenToFloat(), color.blueToFloat(), 1.0,
-        x + width, y + height, 0.0, color.redToFloat(), color.greenToFloat(), color.blueToFloat(), 1.0,
-    };
-
-    _ = quad;
-
-    state.pip = sg.makePipeline(.{
-        .shader = sg.makeShader(shader_quad.quadShaderDesc(sg.queryBackend())),
-        .layout = init: {
-            var l = sg.VertexLayoutState{};
-            l.attrs[shader_quad.ATTR_quad_pos].format = .FLOAT3;
-            l.attrs[shader_quad.ATTR_quad_color].format = .FLOAT4;
-            break :init l;
-        },
-        .index_type = .UINT16,
-    });
-}
-
-pub fn main() void {
+pub fn main() !void {
     sapp.run(.{
         .init_cb = init,
         .frame_cb = frame,
         .cleanup_cb = cleanup,
-        .width = 800,
-        .height = 600,
+        .width = win_width,
+        .height = win_height,
         .icon = .{ .sokol_default = true },
         .window_title = "zigtris",
         .logger = .{ .func = slog.func },
